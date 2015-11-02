a9709b13e7a59ec92b57b27df49a380431201a87
Giau Ngo <giaunv@gmail.com>
2015-11-02 08:55:29 +0700

    Remove author info

--- lib/jekyll_github_sample/reference_tag.rb
+++ lib/jekyll_github_sample/reference_tag.rb
@@ -4,9 +4,6 @@ module JekyllGithubSample
     def render(context)
       <<MARKUP.strip
 <div class="github-sample-reference">
-  <div class="author-info">
-    <a href="#{@github_file.web_uri}">This Github Sample</a> is by <a href="#{@github_file.user_uri}">#{@github_file.user}</a>
-  </div>
   <div class="meta-info">
     #{@github_file.filename} <a href="#{@github_file.web_uri}">view</a> <a href="#{@github_file.raw_uri}">raw</a>
   </div>
